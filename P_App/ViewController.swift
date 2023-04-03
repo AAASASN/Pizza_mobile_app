fd5938da27f96c3b06c71c6b0657c5bff2197a58
AAASASN <maraenko@gmail.com>
2023-04-03 12:35:38 +0300

    Initial Commit

---
+++ P_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  P_App
+//
+//  Created by Александр Мараенко on 03.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
